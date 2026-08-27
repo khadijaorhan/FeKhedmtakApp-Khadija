bebcbdba3b27fc79ae31736898e5f355da7b9321
Ziad Elbahgy <ziadbahgy@gmail.com>
2026-08-27 15:02:07 +0300

    feat: setup Angular project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainLayout } from './layouts/main-layout/main-layout';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: MainLayout
+  }
+
+];
